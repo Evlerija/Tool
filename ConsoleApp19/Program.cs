4478dba9a7285cd14d157cedcffb1e82d6d6b6e5
Kul33 <Kul33@DESKTOP-J8QGFCQ>
2023-02-06 22:44:11 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            drum baraban = new drum(48);
+            guitar gitara = new guitar(5);
+            trumpet truba = new trumpet(7.4f);
+            Tool[] tools = {baraban, gitara, truba };
+            for (int i = 0; i < tools.Length; i++)
+            {
+                tools[i].play();
+
+            }
+        }
+    }
+    public interface Tool
+    {
+
+        void play()
+        {
+            
+        }
+        ////////////////////////////////////////
+        ////////////////////////////////////////
+        ////////////////////////////////////////
+        ////////////////////////////////////////
+        ////////////////////////////////////////
+        //НЕЛЬЗЯ ПРИСВОИТЬ ПЕРЕМЕННУЮ ИНТЕРФЕЙСУ
+        ////////////////////////////////////////
+        ////////////////////////////////////////
+        ////////////////////////////////////////
+        ////////////////////////////////////////
+        ////////////////////////////////////////
+        ////////////////////////////////////////
+    }
+    public class guitar : Tool
+    {
+        public sbyte Str;
+        public guitar(sbyte str)
+        {
+            Str = str;
+        }
+        public void play()
+        {
+            Console.WriteLine("Играет гитара " + "с количеством струн " + "'" + Str + "'");
+        }
+    }
+    
+
+    public class drum : Tool
+    {
+        public int Size;
+        public drum(int size)
+        {
+            Size = size;
+        }
+        public void play()
+        {
+            Console.WriteLine("Играет барабан " + "размером " + "'" + Size + "'");
+        }
+    }
+    public class trumpet : Tool
+    {
+        public float Dm;
+        public trumpet(float dm)
+        {
+            Dm = dm;
+        }
+        public void play()
+        {
+            Console.WriteLine("Играет труба " + "диаметром " + "'" + Dm + "'");
+        }
+    }
+}
